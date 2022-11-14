55470a3623c9b83a12cc81d3ab2514ed1cd169d1
Мартина Пеева <martitopp14@gmail.com>
2022-11-14 19:52:02 +0200

    third merging

---
+++ src/PositionPrison.java
@@ -0,0 +1,11 @@
+public class PositionPrison extends Positions{
+    public PositionPrison(int numberPosition) {
+        super(numberPosition);
+    }
+    @Override
+    public String toString() {
+        return "PositionPrison{" +
+                "numberPosition=" + numberPosition +
+                '}';
+    }
+}
