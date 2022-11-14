4c1e30553b47ef8382311737ca628106c1a78076
kremenakochanova <kremenakochanova@gmail.com>
2022-11-14 22:01:34 +0200

    PositionChanceCard

--- src/Askablе.java
+++ src/Askablе.java
@@ -3,7 +3,9 @@ import java.util.Scanner;
 
 public interface Askablе {
 
-    void askForPayingTheBankAndGetFree(List<Player> players, int i);
+    void seeTheChanceCard(List<Player> players, int i);
+
+    void askForPayingTheBankAndGetFree (List<Player> players, int i);
 
 }
 
--- src/PositionCommunityChestCard.java
+++ src/PositionCommunityChestCard.java
@@ -1,6 +1,7 @@
+import java.util.List;
 import java.util.Scanner;
 
-public class PositionCommunityChestCard extends Positions {
+public class PositionCommunityChestCard extends Positions implements Askablе {
 
     public PositionCommunityChestCard(int numberPosition) {
         super(numberPosition);
@@ -59,4 +60,27 @@ public class PositionCommunityChestCard extends Positions {
     }
 
 
+    @Override
+    public void askForPayingTheBankAndGetFree(List<Player> players, int i) {
+        Scanner scan = new Scanner(System.in);
+        if (players.get(i).getCash() >= 50.0)//check if the player has 50 money
+        {
+            while (true) {
+                System.out.print("Do you want to pay the bank 50 money and not get in jail?\n" +
+                        "Type \"y\" for yes or \"n\" for no:");
+                String option = scan.nextLine();
+                if (option.equalsIgnoreCase("y")) {
+                    players.get(i).setCash(players.get(i).getCash() - 50);
+                    break;
+                } else if (option.equalsIgnoreCase("n")) {
+                    System.out.println(players.get(i).getName() + " goes to jail.");
+                    players.get(i).setCurrentPosition(11);
+                    players.get(i).setBeingInJail(true);
+                    break;
+                } else {
+                    System.out.println("Incorrect input, try again, " + players.get(i).getName() + ".");
+                }
+            }
+        }
+    }
 }
