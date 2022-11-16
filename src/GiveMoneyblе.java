0f9976fa8314b6f2e14361a39f75a652cbcb8e04
kremenakochanova <kremenakochanova@gmail.com>
2022-11-16 23:20:07 +0200

    GiveMoneyblе

--- src/GiveMoneyblе.java
+++ src/GiveMoneyblе.java
@@ -1,9 +1,16 @@
+import java.util.List;
+
 public interface GiveMoneyblе {
 
-    static String[][] giveMoneyToThePlayer(String[][] pPAM, int i, int[][] pWCBBO, int j, int[][] prices) {
-        int sum = prices[2][0] + (prices[2][1] * pWCBBO[2][j]) + (prices[2][2] * pWCBBO[3][j]);
-        pPAM[2][i] = Integer.toString(Integer.parseInt(pPAM[2][i]) + sum);
-        return pPAM;
-    }
+    static void giveMoneyToThePlayer(List<Player> players, int i)
 
+
+
+    {
+        //int sum = prices[2][0] + (prices[2][1] * pWCBBO[2][j]) + (prices[2][2] * pWCBBO[3][j]);
+        //pPAM[2][i] = Integer.toString(Integer.parseInt(pPAM[2][i]) + sum);
+        //return pPAM;
+    }
 }
+
+
