47a54488e6ac18fb5c94d07287196848687b6e17
kremenakochanova <kremenakochanova@gmail.com>
2022-11-20 19:36:47 +0200

    testGetRandom..

--- Test/TestGetRandom.java
+++ Test/TestGetRandom.java
@@ -1,7 +1,5 @@
 import org.junit.Assert;
 import org.junit.Test;
-
-import java.sql.Array;
 import java.util.*;
 
 public class TestGetRandom {
@@ -20,13 +18,14 @@ public class TestGetRandom {
     }
 
 
-    public void testGetRandomFrom0to6WhenPositionPositionChestCard() {
+    @Test
+    public void testGetRandomFrom0to6WhenPositionChangeCard() {
 
-        List<Integer> testList = Arrays.asList(0, 1, 2, 3, 4, 5, 6);
-        PositionCommunityChestCard testPosition = new PositionCommunityChestCard(2);
+        List<Integer> testList1 = Arrays.asList(0, 1, 2, 3, 4, 5, 6);
+        PositionChanceCard testPosition = new PositionChanceCard(10);
 
-        for (int i = 0; i < 7; i++) {
-            Assert.assertTrue("Method doesn't return expected value",testList.contains(testPosition.getRandomFrom0To6()));
+        for (int i = 0; i <= 7; i++) {
+            Assert.assertTrue("Method doesn't return expected value",testList1.contains(testPosition.getRandomFrom0To6()));
         }
     }
 }
