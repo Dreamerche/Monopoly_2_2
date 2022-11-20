b177fe16b244e396d86e643bdea6508ddeae80f7
kremenakochanova <kremenakochanova@gmail.com>
2022-11-20 17:19:40 +0200

    Test by metods

--- Test/TestaskForPayingTheBankAndGetFree.java
+++ Test/TestaskForPayingTheBankAndGetFree.java
@@ -5,8 +5,36 @@ import java.util.Scanner;
 public class TestaskForPayingTheBankAndGetFree {
     Scanner scan = new Scanner(System.in);
 
-    @Test
+ @Test
 
+//    assertTrue
+//    public class CalculatorTest {
+//        private Calculator calc = new Calculator();
+
+//        //in this case test passes
+//        @Test
+//        public void testAreEqualWhenInputNumbersAreSame(){
+//            Assert.assertTrue(“Method doesn’t
+//            return expected value”, calc.areEqual(1, 1));
+//        }
+//    assertFalse(String message, boolean condition) - за проверка дали дадено условие е лъжа
+//    assertNull(String message, Object o) - за проверка дали даден обект е Null
+//    assertNotNull(String message, Object o) - за проверка дали даден обект не е Null
+//    assertArrayEquals(String message, expected, actual) - за сравнение на масиви по дължина и съдържание
+//    assertEquals(String message, double expected, double actual, double delta) - за сравнение на
+//    double, delta - точност след запетаята
+        //@Test
+         ///public void givenRadius_whenCalculateArea_thenReturnArea() {
+         //    double actualArea = Circle.calculateArea(1d);
+         ///    double expectedArea = 3.141592653589793;
+        ///    Assert.assertEquals(expectedArea, actualArea);
+        ///}
+            //@Test
+            //public void givenRadius_whenCalculateArea_thenReturnArea() {
+            //    double actualArea = Circle.calculateArea(2d);
+            //    double expectedArea = 3.141592653589793 * 2 * 2;
+            //    Assert.assertEquals(expectedArea, actualArea);
+            //}
 
 
     }
