d781b4ea0b2343cca79dbc4cf1e078b84626ad43
Мартина Пеева <martitopp14@gmail.com>
2022-11-24 22:24:51 +0200

    all methods working, need more tests

--- src/PositionForBuilding.java
+++ src/PositionForBuilding.java
@@ -12,46 +12,88 @@ public class PositionForBuilding extends Position implements RentAblе {
     private int owner;
     private int amountOfHouses;
     private int amountOfHotels;
+
     @Override
     public void seeWhatThePositionOffersOrTakes(List<Player> players, int i, List<Position> positions) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         if (isTheBankTheOwner()) {
-            if(askForBuyingThePlace(players,i)){
-                System.out.println(buyThePlace(players,i));
+            if (askForBuyingThePlace(players, i)) {
+                System.out.println(buyThePlace(players, i));
             }
-        }
-        else if(getOwner()==i){
-            if(amountOfHouses<=4){
-            System.out.print(players.get(i).getName()+", do you want to build a hotel? Type \"y\" for yes or \"n\" for no: ");
-
+        } else if (getOwner() == i) {
+            if (amountOfHouses < 4 && players.get(i).getCash() >= buyAHouse && askForBuyingAHouse(players, i)) {
+                System.out.println(buildHouseOnPosition(players, i));
             }
-            offerToBuild(players,i);
-        }
-        else {
+            if (amountOfHotels == 0 && players.get(i).getCash() >= buyAHotel && askForBuyingAHotel(players, i)) {
+                System.out.println(buildHotelOnPosition(players, i));
+            }
+        } else {
             System.out.println(payTheOwner(players, i));
         }
-        System.out.println("Working on this method");
+        System.out.println();
     }
-    protected String offerToBuild(List<Player> players,int i){
-        return "done"
+
+    protected String buildHouseOnPosition(List<Player> players, int i) {
+        setAmountOfHouses(getAmountOfHouses() + 1);
+        players.get(i).setCash(players.get(i).getCash() - buyAHouse);
+        return (players.get(i).getName() + " bought a house.");
     }
-    @Override
-    public String payTheOwner(List<Player> players,int i){
-        int sum=rentAPosition+(rentAHouse*amountOfHouses)+(rentAHotel*amountOfHotels);
-        players.get(owner).setCash(players.get(getOwner()).getCash()+sum);
-        players.get(i).setCash(players.get(i).getCash()-sum);
-        return (players.get(i).getName()+" gave "+sum+" rent to "+players.get(owner).getName()+".");
+
+    protected String buildHotelOnPosition(List<Player> players, int i) {
+        setAmountOfHotels(1);
+        players.get(i).setCash(players.get(i).getCash() - buyAHotel);
+        return (players.get(i).getName() + " bought a hotel.");
     }
-    protected String buyThePlace(List<Player> players,int i){
+
+    protected String buyThePlace(List<Player> players, int i) {
         setOwner(i);
-        players.get(i).setCash(players.get(i).getCash()-buyAPosition);
-        return ("The place on position "+getNumberPosition()+" is bought by "+players.get(i).getName()+".");
+        players.get(i).setCash(players.get(i).getCash() - buyAPosition);
+        return ("The place on position " + getNumberPosition() + " is bought by " + players.get(i).getName() + ".");
     }
+
+    @Override
+    public String payTheOwner(List<Player> players, int i) {
+        int sum = rentAPosition + (rentAHouse * amountOfHouses) + (rentAHotel * amountOfHotels);
+        players.get(owner).setCash(players.get(getOwner()).getCash() + sum);
+        players.get(i).setCash(players.get(i).getCash() - sum);
+        return (players.get(i).getName() + " gave " + sum + " rent to " + players.get(owner).getName() + ".");
+    }
+
     protected boolean isTheBankTheOwner() {
         if (owner == -1) {
             return true;
         } else {
             return false;
+        }
+    }
+
+    public boolean askForBuyingAHouse(List<Player> players, int i) {
+        Scanner scan = new Scanner(System.in);
+        while (true) {
+            System.out.print(players.get(i).getName() + ", do you want to buy a house on this position, " + getNumberPosition() + ", for " + buyAHouse + "? Type \"y\" for yes or \"n\" for no: ");
+            String answer = scan.nextLine();
+            if (answer.equalsIgnoreCase("y")) {
+                return true;
+            } else if (answer.equalsIgnoreCase("n")) {
+                return false;
+            } else {
+                System.out.println("Incorrect input, try again, " + players.get(i).getName() + ".");
+            }
+        }
+    }
+
+    public boolean askForBuyingAHotel(List<Player> players, int i) {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.print(players.get(i).getName() + ", do you want to buy a hotel on this position, " + getNumberPosition() + ", for " + buyAHotel + "? Type \"y\" for yes or \"n\" for no: ");
+            String answer = sc.nextLine();
+            if (answer.equalsIgnoreCase("y")) {
+                return true;
+            } else if (answer.equalsIgnoreCase("n")) {
+                return false;
+            } else {
+                System.out.println("Incorrect input, try again, " + players.get(i).getName() + ".");
+            }
         }
     }
 
@@ -72,6 +114,7 @@ public class PositionForBuilding extends Position implements RentAblе {
         }
         return false;
     }
+
     public PositionForBuilding(int numberPosition) {
         super(numberPosition);
         this.owner = -1;
